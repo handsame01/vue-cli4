d9c1f815953a21b27249c27a2509b61393ec402c
xzw <2311075864@qq.com>
2020-03-07 14:18:14 +0800

    rem适配

--- vue-cli4/postcss.config.js
+++ vue-cli4/postcss.config.js
@@ -1,21 +1,21 @@
-module.exports = {
-    plugins: {
-        "autoprefixer": {},
-        'postcss-px-to-viewport': {
-            unitToConvert: 'px',
-            viewportWidth: 320,
-            unitPrecision: 5,
-            propList: ['*'],
-            viewportUnit: 'vw',
-            fontViewportUnit: 'vw',
-            selectorBlackList: [],
-            minPixelValue: 1,
-            mediaQuery: false,
-            replace: true,
-            exclude: [],
-            landscape: false,
-            landscapeUnit: 'vw',
-            landscapeWidth: 568
-        }
-    }
-}
+// module.exports = {      这个是把px自动转换成vw的
+//     plugins: {
+//         "autoprefixer": {},
+//         'postcss-px-to-viewport': {
+//             unitToConvert: 'px',
+//             viewportWidth: 320,
+//             unitPrecision: 5,
+//             propList: ['*'],
+//             viewportUnit: 'vw',
+//             fontViewportUnit: 'vw',
+//             selectorBlackList: [],
+//             minPixelValue: 1,
+//             mediaQuery: false,
+//             replace: true,
+//             exclude: [],
+//             landscape: false,
+//             landscapeUnit: 'vw',
+//             landscapeWidth: 568
+//         }
+//     }
+// }
--- vue-cli4/src/main.js
+++ vue-cli4/src/main.js
@@ -3,14 +3,24 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from "axios";
-const HOST=process.env.VUE_APP_BASE_API
+const HOST = process.env.VUE_APP_BASE_API
 axios.defaults.baseURL = HOST;
 window.axios = axios;
 // window.HOST = HOST;
 Vue.config.productionTip = false
-window.EvnetBus=new Vue()
+window.EvnetBus = new Vue()
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+router.beforeEach((to, from, next) => {
+  next()
+})
+window.onresize = () => {
+  console.log('改变了屏幕尺寸')
+  window.Size = document.getElementsByTagName('html')[0].style.fontSize
+  var clientWidth = document.body.clientWidth
+  window.Size = (clientWidth/375)*10+'px'
+  console.log(window.Size)
+}
